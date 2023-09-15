3f9ab78043f48fe4092c0c9c9cc907629598a942
MediaLab <MediaLab@DESKTOP-9836970>
2023-09-15 10:20:12 +0900

    boolean type test

---
+++ HelloWorld/src/BooleanTypeTest.java
@@ -0,0 +1,14 @@
+
+public class BooleanTypeTest {
+
+	public static void main(String[] args) {
+		boolean a = true; //save true
+		System.out.println("boolean 변수 adml rkqtdms : " + a);
+		boolean b = 10 > 20;//save result of 10>20
+		System.out.println("boolean 변수 b의 값은 : " + b);
+		boolean c = a;//save boolean 
+		System.out.println("boolean 변수 c의 값은 : " + c);
+
+	}
+
+}
