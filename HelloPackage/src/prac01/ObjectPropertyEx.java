c54936135b9e0014883c413fd6408394fd111039
user <user@DESKTOP-31IHISG>
2023-10-02 22:07:16 +0900

    231002 object class 연습

---
+++ HelloPackage/src/prac01/ObjectPropertyEx.java
@@ -0,0 +1,56 @@
+package prac01;
+
+class Point{
+	private int x, y;
+	public Point(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	//override toString() that returns string of Point object
+	public String toString() {
+		return "Point(" + x + "," + y +")";
+	}
+	
+	//implement equals() / compare two point
+	public boolean equals(Object obj) {
+		Point p = (Point)obj;
+		if(x == p.x && y == p.y) return true;
+		else return false;
+	}
+}
+
+public class ObjectPropertyEx {
+	public static void print(Object obj) {
+		 System.out.println(obj.getClass().getName());//클래스명
+		 System.out.println(obj.hashCode());		  //해시코드 값
+		 System.out.println(obj.toString());		  //객체를 문자열로 만들어 출력
+		 System.out.println(obj);					  //객체 출력
+	}
+	
+	public static void main(String[] args) {
+		Point p = new Point(2, 3);
+		print(p);
+		
+		//call toString() method
+		 System.out.println(p.toString());
+		 System.out.println(p);
+		 System.out.println(p + "입니다.");
+		 
+		 //test equals() method
+		 Point a = new Point(2,3);
+		 Point b = new Point(2,3);
+		 Point c = new Point(3,4);
+		 
+		 if(a == b) { //false
+			 System.out.println("a == b");
+		 }
+		 if(a.equals(b)) { //true
+			 System.out.println("a is equal to b");
+		 }
+		 if(a.equals(c)) { //false
+			 System.out.println("a is equal to c");
+		 }
+	}
+
+}
