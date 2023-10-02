7e54f474007d0dc48b0ee6db0446ae101057eb58
user <user@DESKTOP-31IHISG>
2023-10-02 16:07:26 +0900

    231002 7주차 온라인 enum 강의내용

---
+++ HelloWorld/src/EnumEx.java
@@ -0,0 +1,46 @@
+
+public class EnumEx {
+
+	public static void main(String[] args) {
+		enum Week{월, 화, 수, 목, 금, 토, 일};
+		
+		//values() method returns all enumerated elements in an array in sequential order
+		for(Week day : Week.values())
+			System.out.println(day + "요일 ");
+		System.out.println();
+		
+		// ordinal() returns the enumerated order of an element as an integer value
+		//index value starts form 0 and values incr3ease sequentially
+		for(Week day : Week.values())
+			System.out.println(day + "요일 has value " + day.ordinal());
+		System.out.println();
+		
+		// valueOf() returns the element in the enumeration
+		// that has a name matching the String provided as a parameter
+		Week weekTest1 = Week.목;
+		Week weekTest2 = Week.valueOf("토");
+		System.out.println(weekTest1);
+		System.out.println(weekTest2);
+		System.out.println();
+		
+		// associate characters with constants, you should use a semicolon
+		enum WeekEng{
+			월("Mon"), 화("Tue"), 수("Wed"), 목("Thu"), 금("Fri"), 토("Sat"), 일("Sun");
+			
+			final private String engName;
+			private WeekEng(String name) {
+				this.engName = name;
+			}
+			
+			public String getName() {
+				return engName;
+			}
+		};
+		for (WeekEng day : WeekEng.values()) {
+			System.out.println(day.getName());
+		}
+		
+
+	}
+
+}
