19f0b7acac4d3181e1280369b14c4ea17e76f6ef
user <user@DESKTOP-31IHISG>
2023-10-02 16:06:31 +0900

    231002 7주차 ArrayException 강의 내용 포함

--- HelloWorld/src/ArrayException.java
+++ HelloWorld/src/ArrayException.java
@@ -5,13 +5,34 @@ public class ArrayException {
 		int[] intArray = new int[5];
 		intArray[0] = 0;
 		
-		try {
-			for(int i =0; i<5;i++) {
+		for(int i =0; i<5;i++){
+			try{
 				intArray[i+1] = i + 1 + intArray[i];
 				System.out.println("intArray["+i+"} = "+intArray[i]);
+				
+				if(i == 2) {
+					System.out.println(intArray[i]/0);
+				}
+				
+				//intentionally raise an exception
+				if(i == 1) {
+					ArithmeticException e = new ArithmeticException("intentionally raise");
+					throw e;
+					//throw new ArithemeticException("intentionally raise");
+				}
+				
+				if(i == 0) {
+					throw new MyException("myException");
+				}
+			}catch(ArrayIndexOutOfBoundsException e1) {
+				System.out.println("Exception: ArrayIndexOutOfBounds");
+			}catch(ArithmeticException e2) {
+				System.out.println("0");
+				System.out.println(e2.getMessage());
+				e2.printStackTrace();
+			}catch (MyException e3) {
+				e3.printStackTrace();
 			}
-		}catch(ArrayIndexOutOfBoundsException e) {
-			System.out.println("Exception: ArrayIndexOutOfBounds");
 		}
 
 	}
