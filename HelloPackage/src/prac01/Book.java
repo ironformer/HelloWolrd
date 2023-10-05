d0c67f71a11c1090142ede850efad6e09a7396a5
MediaLab <MediaLab@DESKTOP-9836970>
2023-10-05 14:48:39 +0900

    231005 수업시간 생성 클래스

---
+++ HelloPackage/src/prac01/Book.java
@@ -0,0 +1,20 @@
+package prac01;
+
+public class Book {
+	
+	
+	public Book(String t) {
+		
+	}
+	public Book(String t, String a) {
+		
+	}
+	
+
+	public static void main(String[] args) {
+		Book littlePrince = new Book("어린왕자", "생텍쥐페리");
+		
+
+	}
+
+}
